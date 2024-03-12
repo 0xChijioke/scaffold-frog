11dce9ff2e31a1034e902c62f5c6af8864d3cfc6
EngrGord <chijokeogbuanya@gmail.com>
2024-03-12 10:16:52 +0100

    format

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -1,24 +1,18 @@
 import "@rainbow-me/rainbowkit/styles.css";
-import { getFrameMetadata } from 'frog/next'
+import { getFrameMetadata } from "frog/next";
 import { Metadata } from "next";
 import { ScaffoldEthAppWithProviders } from "~~/components/ScaffoldEthAppWithProviders";
 import { ThemeProvider } from "~~/components/ThemeProvider";
 import "~~/styles/globals.css";
-
-
 
 const baseUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : `http://localhost:${process.env.PORT || 3000}`;
 const imageUrl = `${baseUrl}/thumbnail.jpg`;
 
-
 export async function generateMetadata(): Promise<Metadata> {
-  
-  const frameTags = await getFrameMetadata(
-    `${process.env.VERCEL_URL || 'http://localhost:3000'}/api`,
-  )
-  
+  const frameTags = await getFrameMetadata(`${process.env.VERCEL_URL || "http://localhost:3000"}/api`);
+
   return {
     metadataBase: new URL(baseUrl),
     title: {
@@ -51,9 +45,8 @@ export async function generateMetadata(): Promise<Metadata> {
       icon: [{ url: "/favicon.png", sizes: "32x32", type: "image/png" }],
     },
     other: frameTags,
-  }
+  };
 }
-
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
   return (
